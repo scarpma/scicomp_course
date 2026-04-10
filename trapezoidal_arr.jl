6491977b8a3b2ff6c7fa797e5e3cf8daabefe895
scarpma <scarpma@gmail.com>
2026-04-10 12:08:13 +0200

    julia arrays

---
+++ trapezoidal_arr.jl
@@ -0,0 +1,43 @@
+using Printf
+
+f(x) = x^3
+
+function trap(a::Float64, b::Float64, n::Int,
+    x_values::Vector{Float64},
+    f_values::Vector{Float64}
+    )
+
+    dx = (b-a)/n
+    s = 0.5*(f(a) + f(b))
+    x_values[1] = a
+    f_values[1] = f(x_values[1])
+
+    @inbounds for i in 2:n
+        x_values[i] = a + (i - 1) * dx
+        f_values[i] = f(x_values[i])
+        s += f_values[i]
+    end
+
+    return s * dx
+end
+
+n = 1000
+a, b = 0.0, 1.0
+
+# heap allocated arrays! static arrays require finer control --> not useful for now
+# undef stands for uninitialized memory
+x_values = Vector{Float64}(undef, n)
+f_values = Vector{Float64}(undef, n)
+
+@printf("numerical integration of f(x)=x^3 from a=%.2f to b=%.2f\n", a, b)
+
+result = trap(a, b, n, x_values, f_values)
+result_an = 0.25 * (b^4 - a^4)
+
+@inbounds for i in 1:n
+    @printf("x[%4d]=%.5f, f(x[%4d])=%.5f\n", i-1, x_values[i], i-1, f_values[i])
+end
+
+@printf("         result=%.4f\n", result)
+@printf("analytic result=%.4f\n", result_an)
+@printf("      abs error=%.2e\n", abs(result - result_an))
